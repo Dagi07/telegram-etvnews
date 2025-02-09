e3116b204084116af1534f88190e68bf5a1044cf
Dagi07 <dagmawiamanuel@gmail.com>
2025-02-09 23:30:24 +0300

    added cron job script

--- app.py
+++ app.py
@@ -1,37 +1,34 @@
 from dotenv import load_dotenv
 from http.server import HTTPServer, BaseHTTPRequestHandler
-# from flask import Flask, request, jsonify
-# from main import main
 import subprocess
 import os
 
 load_dotenv()
 
-# Access the variables
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def getRequest():
-#     print("requet came")
-#     return 200
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 HOST = os.getenv('HOST')
 PORT = os.getenv('PORT')
+
 class NeuralHTTP(BaseHTTPRequestHandler):
     def do_GET(self):
-        print("\n request came \n")
-        subprocess.Popen(["python3", "main.py"])
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><body><h1>Sending News</h1></body></html>", "utf-8"))
+        print("\nRequest came for:", self.path, "\n")
+        if self.path == '/run-script':
+            # Execute the cronJob.sh script when the URL is /run-script
+            subprocess.Popen(["bash", "cronJob.sh"])
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"<html><body><h1>Cron Job Script Executed</h1></body></html>")
+        else:
+            # Default: execute main.py
+            subprocess.Popen(["python3", "main.py"])
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"<html><body><h1>Sending News</h1></body></html>")
 
-server = HTTPServer((HOST, int(PORT)), NeuralHTTP)
-print("\n Server running on ", PORT)
-server.serve_forever()
-server.server_close()
-print("Server now stopped")
+if __name__ == "__main__":
+    server = HTTPServer((HOST, int(PORT)), NeuralHTTP)
+    print("\nServer running on", PORT)
+    server.serve_forever()
+    server.server_close()
+    print("Server now stopped")
